c5bbf296cf6919ea115ccd698d9bcf84cba4ddbc
jaychenfe <chenjiehwy@163.com>
2023-05-11 22:03:38 +0800

    add 03312

--- questions/03312-easy-parameters/template.ts
+++ questions/03312-easy-parameters/template.ts
@@ -1,1 +1,6 @@
-type MyParameters<T extends (...args: any[]) => any> = T extends (infer R )=>any?R:never
+type MyParameters<T extends (...args: any[]) => any> = T extends (
+  ...args: infer R
+) => any
+  ? R
+  : never
+
